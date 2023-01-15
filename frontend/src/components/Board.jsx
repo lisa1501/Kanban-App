418e31349019f9aed8a4f1e0810b128feb1c57cf
halisa <halisasaipulaa@gmail.com>
2023-01-15 14:57:45 -0800

    Handing On Drag End Events-- successfullly drag single task from one tasks list to another one

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -21,8 +21,69 @@ function Board(props) {
         return data.board;
     }
 
-    function onDragEnd() {
-        alert('dropped!');
+    function onDragEnd(result) {
+        const {destination, source, draggableId, type} = result;
+        if (!destination) {
+            return ;
+        }
+        if (destination.droppableId === source.droppableId && destination.index === source.index) {
+            return ;
+        }
+
+        if (type === 'column') {
+            const newColumnOrder = Array.from(board.columnOrder);
+            newColumnOrder.splice(source.index, 1);
+            newColumnOrder.splice(destination.index, 0, draggableId)
+            
+            setBoard({
+                ...board,
+                columnOrder:newColumnOrder,
+            })
+            return ;
+        }
+        const start = board.columns[source.droppableId];
+        const finish = board.columns[destination.droppableId];
+        if (start === finish) {
+            const newTaskIds = Array.from(start.taskIds);
+            newTaskIds.splice(source.index,1)
+            newTaskIds.splice(destination.index, 0, draggableId)
+
+            const newColumn = {
+                ...start,
+                taskIds:newTaskIds
+
+            }
+            setBoard({
+                ...board,
+                columns: {
+                    ...board.columns,
+                    [newColumn.id]: newColumn
+                }
+            });
+            return ;
+        }
+        const startTaskIds = Array.from(start.taskIds);
+        startTaskIds.splice(source.index, 1);
+        const newStartColumn = {
+            ...start,
+            taskIds: startTaskIds,
+        }
+        const finishTaskIds = Array.from(finish.taskIds)
+        finishTaskIds.splice(destination.index, 0, draggableId)
+
+        const newFinishColumn = {
+            ...finish,
+            taskIds: finishTaskIds
+        }
+        setBoard({
+            ...board,
+            columns:{
+                ...board.columns,
+                [newStartColumn.id]: newStartColumn,
+                [newFinishColumn.id]: newFinishColumn
+            }
+        });
+        return ;
     }
     return (
         <DragDropContext onDragEnd={onDragEnd}>
