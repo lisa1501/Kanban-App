1ac55692c4935fd9d96c5a2a667c7369234585b8
halisa <halisasaipulaa@gmail.com>
2023-01-16 10:42:15 -0800

    Created Pydantic Schema and User Model

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,42 @@
-from  fastapi import FastAPI
+from fastapi import FastAPI
+from pydantic import BaseModel
+from tortoise import fields
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
+from tortoise.models import Model
+
 
 app = FastAPI()
 
+class Task(BaseModel):
+    id: str
+    content:str
+
+class Tasks(BaseModel):
+    __root__:dict[str, Task]
+
+class Column(BaseModel):
+    id: str
+    title: str
+    taskIds: list
+
+class Columns(BaseModel):
+    __root__: dict[str, Column]
+
+
+class Board(BaseModel):
+    tasks: Tasks
+    column: Columns
+    columnOrder:list
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    username = fields.CharField(50, unique=True)
+    password = fields.CharField(200)
+    board = fields.JSONField(default={"tasks":{}, "columns":{}, "columnOrder":[]})
+
+User_Pydantic = pydantic_model_creator(User, name='User')
+UserIn_Pydantic = pydantic_model_creator(User, name="UserIn", exclude_readonly= True, exclude=('board',))
 @app.get('/board')
 def get_board():
     board_data = {
@@ -12,7 +47,7 @@ def get_board():
         'columns': {
             'column-1':{
                 'id':'column-1',
-                'title':'to do',
+                'title':'To Do',
                 'taskIds':['task-2','task-3']
             },
             'column-2':{
