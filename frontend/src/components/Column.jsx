87267d51e41190253068f19b587fec0829dede72
halisa <halisasaipulaa@gmail.com>
2023-01-16 10:05:58 -0800

    implemented delete column and task

--- frontend/src/components/Column.jsx
+++ frontend/src/components/Column.jsx
@@ -24,17 +24,43 @@ const TaskList = styled.div`
 `;
 
 function Column(props) {
+    function deleteColumn(columnId, index) {
+        const columnTasks = props.board.columns[columnId].taskIds;
+        console.log(columnTasks)
+        const finalTasks = columnTasks.reduce((previousValue, currentValue) => {
+            const {[currentValue]: oldTask, ...newTasks} = previousValue;
+            // console.log(finalTasks)
+            return newTasks;
+        }, props.board.tasks);
+        // console.log(finalTasks)
+        const columns = props.board.columns;
+        const {[columnId]: odlColumn, ...newColumns} = columns;
+        const newColumnsOrder = Array.from(props.board.columnOrder)
+        newColumnsOrder.splice(index, 1);
+        props.setBoard({
+            tasks:{
+                ...finalTasks,
+            },
+            columns:{
+                ...newColumns,
+            },
+            columnOrder: newColumnsOrder
+
+        });
+    }
     return (
         <Draggable draggableId={props.column.id} index={props.index}>
             {provided => (
                 <Container {...provided.draggableProps} ref={provided.innerRef}>
-                    <Title {...provided.dragHandleProps}>{props.column.title}</Title>
+                    <Title {...provided.dragHandleProps}>{props.column.title}
+                        <span onClick={() => deleteColumn(props.column.id, props.index)}>X</span>
+                    </Title>
                     <Droppable droppableId={props.column.id} type="task">
                         {provided => (
                         <TaskList {...provided.droppableProps} ref={provided.innerRef}>
                             {
                                 props.tasks.map((task, index) => 
-                                    (<Task key={task.id} task={task} index={index} columnId={props.column.id} />)
+                                    (<Task key={task.id} task={task} index={index} columnId={props.column.id} board={props.board} setBoard={props.setBoard} />)
                                 )
                             }
                             {provided.placeholder}
