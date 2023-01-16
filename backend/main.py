391e9fda2d90ccba823352c017a68a01b94efcf3
Lisa S <halisasaipulla@gmail.com>
2023-01-16 11:29:36 -0800

    successfully Connected App to Database

--- backend/main.py
+++ backend/main.py
@@ -38,25 +38,14 @@ class User(Model):
 User_Pydantic = pydantic_model_creator(User, name='User')
 UserIn_Pydantic = pydantic_model_creator(User, name="UserIn", exclude_readonly= True, exclude=('board',))
 @app.get('/board')
-def get_board():
-    board_data = {
-        'tasks': {'task-1':{'id':'task-1','content':'create video'},
-                    'task-2':{'id':'task-2','content':'edit video'},
-                    'task-3':{'id':'task-3','content':'publish video'}, 
-        },
-        'columns': {
-            'column-1':{
-                'id':'column-1',
-                'title':'To Do',
-                'taskIds':['task-2','task-3']
-            },
-            'column-2':{
-                'id':'column-2',
-                'title':'Done',
-                'taskIds':['task-1']
-            }
+async def get_board():
+    user = await User.get(id=1)
+    return {'board':user.board}
 
-        },
-        'columnOrder': ['column-1','column-2']
-    }
-    return {'board':board_data}
+register_tortoise(
+    app, 
+    db_url='postgres://postgres:Password1@127.0.0.1:5432/postgres',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
